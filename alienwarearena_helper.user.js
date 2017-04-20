eecd4b14ce4f1e7a5380058dfda6083160acfa8a
thomas-ashcraft <ashcraftom@gmail.com>
2017-04-21 00:18:26 +0300

    0.2.0
    Totally new algorithm for making content votes, and avoid microburst load on the server

--- alienwarearena_helper.user.js
+++ alienwarearena_helper.user.js
@@ -1,7 +1,7 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name         Alienware Arena helper
 // @namespace    https://github.com/thomas-ashcraft
-// @version      0.1.0
+// @version      0.2.0
 // @description  Earn daily ARP easily
 // @author       Thomas Ashcraft
 // @match        *://*.alienwarearena.com/*
@@ -20,15 +20,13 @@
 	path = path.replace(/\/+/g, "/");
 	if(DEBUG) console.log("🐾 path: " + path);
 
-	var referrer = document.referrer;
-	if(DEBUG) console.log("🐾 referrer: " + referrer);
-
 	// Embed style
 	var helper_style = `
 		#background {}
 		.awah-btn-tots {background-color: #f05000;}
-		.awah-arp-pts {float: left; clear: both; font-size: smaller !important;}
 		.awah-btn-cons {color: gold;}
+		.awah-arp-pts {float: left; clear: both; font-size: smaller !important;}
+		.awah-arp-status {float: left; clear: both; font-size: smaller !important;}
 		`;
 	document.head.appendChild(document.createElement('style')).innerHTML=helper_style.replace(/([\s\S]*?return;){2}([\s\S]*)}/,'$2');
 
@@ -40,13 +38,11 @@
 	votes_tot_max = parseInt(pm_counter[4], 10);
 	votes_content_promised = 0;
 	votes_content_action = false;
-	//$("div.toast-header").append('<span class="toast-title awah-arp-pts" style="float: left; clear: both;">asdf</span>');
-	//$("head").html($("head").html().replace('<span data-notify="title" class="toast-title">ARP STATUS <span class="odometer" id="arp-status-total"></span></span>',
-	//	'<span data-notify="title" class="toast-title">ARP STATUS <span class="odometer" id="arp-status-total"></span></span> <span class="toast-title awah-arp-pts" style="float: left; clear: both;"></span>'));
-	//$("div.toast-header").text("CON: " + votes_content_cur + " / " + votes_content_max + " | TOT: " + votes_tot_cur  + " / " + votes_tot_max);
-	//$(".awah-arp-pts").html("CON: " + votes_content_cur + " / " + votes_content_max + " <br> TOT: " + votes_tot_cur  + " / " + votes_tot_max);
+	votes_content_processing = false;
+	votes_content_gathering = false;
+	content_to_vote = [];
 	setTimeout(function() {
-		$("div.toast-header").append('<span class="toast-title awah-arp-pts">asdf</span>');
+		$("div.toast-header").append('<span class="toast-title awah-arp-pts"></span><span class="toast-title awah-arp-status"></span>');
 		arp_pts_status_update();
 	}, 1);
 
@@ -68,7 +64,6 @@
 	});
 	
 	function arp_pts_status_update() {
-		//$("div.toast-header").text("CON: " + votes_content_cur + " / " + votes_content_max + " | TOT: " + votes_tot_cur  + " / " + votes_tot_max);
 		$(".awah-arp-pts").html("CON: " + votes_content_cur + " / " + votes_content_max + " <br> TOT: " + votes_tot_cur  + " / " + votes_tot_max);
 	}
 
@@ -105,7 +100,7 @@
 
 	function this_or_that_btn() {
 		$(".btn-show-vote").clone().removeClass("btn-show-vote").addClass("awah-btn-tots").appendTo(".btn-group-sm");
-		$(".awah-btn-tots span.hidden-xs").text("Spam TOT votes");
+		$(".awah-btn-tots span.hidden-xs").text("Make TOT votes");
 		$(".awah-btn-tots").on("click", function() {
 			$(".awah-btn-tots").addClass("hidden");
 			this_or_that_spam(); //show must go on
@@ -115,27 +110,27 @@
 	function votes_content_spam() {
 		// i.fa-arrow-up not style="color: gold;" //comment
 		// i.fa-chevron-up not style="color: gold;" //post
-		//votes_content_promised = votes_content_cur;
 
+		// TODO: filter to avoid arrow-down arleady clicked comments
 		$("i.fa-arrow-up:not([style='color: gold;'])").each(function( index ) {
 			if ((votes_content_cur + votes_content_promised) < votes_content_max) {
 				votes_content_promised++;
 				this.click();
-				$(".awah-arp-pts").text("votes incoming: " + votes_content_promised);
+				$(".awah-arp-status").text("votes incoming: " + votes_content_promised);
 			} else {
-				$(".awah-arp-pts").html("votes incoming: " + votes_content_promised + "<br>Enough votes");
+				$(".awah-arp-status").html("votes incoming: " + votes_content_promised + " / ENOUGH");
 				votes_content_action = false;
 				return false;
 			}
 		});
 		if (votes_content_promised == 0 && votes_content_action) {
-			$(".awah-arp-pts").text("Getting to the next page");
+			$(".awah-arp-status").text("Getting to the next page");
 			votes_content_next_page();
 		}
 	}
 
 	function votes_content_next_page() {
-		$("ul.pagination > li.next > a").filter(":first").click();
+		$("ul.pagination > li.next:not(.disabled) > a").filter(":first").click();
 	}
 
 	function votes_content_algorithm() {
@@ -162,13 +157,118 @@
 		votes_content_spam();
 	}
 
+	// NEW !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	function votes_content_make() {
+		//$('.post-up-vote') on click
+		//var postId = $(this).data('post-id');
+		votes_content_processing = true;
+		var url = content_to_vote.shift();
+		//var url    = "/forums/post/up-vote/replaceMe";
+		//url        = url.replace('replaceMe', postId);
+		var postId = url.replace("/forums/post/up-vote/", "");
+
+		$.ajax({
+			url: url,
+			type: 'post',
+			success: function(data){
+				if (data.success) {
+                    $('.post-vote-count[data-post-id="'+postId+'"]').html(data['voteTotal']);
+
+                    if (data.votedForContent) {
+                        $('#post-'+postId+' .post-up-vote .fa-arrow-up').css('color', 'gold');
+                    } else {
+                        $('#post-'+postId+' .post-up-vote .fa-arrow-up').css('color', '#c8c8c8');
+                    }
+
+                    $('#post-'+postId+' .post-down-vote .fa-arrow-down').css('color', '#c8c8c8');
+                }
+				if (content_to_vote.length > 0) {
+					votes_content_make();
+				} else {
+					votes_content_processing = false;
+				}
+			},
+			error: function(data){
+				if (content_to_vote.length > 0) {
+					votes_content_make();
+				} else {
+					votes_content_processing = false;
+				}
+			},
+        });
+	}
+
+	function votes_content_gather() {
+		votes_content_gathering = true;
+
+		// TODO: filter to avoid arrow-down arleady clicked comments
+		$("i.fa-arrow-up:not([style='color: gold;'])").each(function( index ) {
+			if ((votes_content_cur + content_to_vote.length) < votes_content_max) {
+				var url = "/forums/post/up-vote/" + $(this).parent().attr('data-post-id');
+				content_to_vote.push(url);
+				$(".awah-arp-status").text("votes incoming: " + content_to_vote.length);
+			} else {
+				votes_content_gathering = false;
+				$(".awah-arp-status").html("votes incoming: " + content_to_vote.length + " / ENOUGH");
+				return false;
+			}
+		});
+		if (content_to_vote.length > 0 && !votes_content_processing) {
+			votes_content_make();
+		}
+		if (votes_content_gathering) {
+			$(".awah-arp-status").text("Getting to the next page");
+			votes_content_get_next_page();
+		}
+	}
+
+	function votes_content_get_next_page() {
+		//$('.pagination li a').on('click'
+		//e.preventDefault();
+		//$ele         = $(this);
+		var href     = $("ul.pagination > li.next:not(.disabled) > a").filter(":first").attr('href');
+		var parts    = href.split('/');
+		var page     = parts.pop();
+		var entityId = parts.pop();
+
+		if (href) {
+			$.ajax({
+				url: href,
+				type: 'get',
+				success: function(data) {
+					$('#reply-wrapper').empty();
+					$('#reply-wrapper').append(data);
+					$('.ucf-comments .timeago').timeago();
+					votes_content_gather();
+				},
+				error: function(data) {}
+			});
+		} else {
+			votes_content_gathering = false;
+		}
+	}
+
 	function votes_content_btn() {
 		$('<a class="btn btn-default text-uppercase awah-btn-cons" href="javascript:void(0);">' +
 			'<i class="fa fa-arrow-up" style="color: gold;"></i> ' +
-			'<span class="hidden-xs">Spam CON votes</span></a>').appendTo(".btn-group-sm");
+			'<span class="hidden-xs">Make CON votes</span></a>').appendTo(".btn-group-sm");
 		$(".awah-btn-cons").on("click", function() {
 			$(".awah-btn-cons").addClass("hidden");
-			votes_content_algorithm(); //show must go on
+			//votes_content_algorithm(); //show must go on
+			votes_content_gather(); //new algorithm
+		});
+	}
+
+	function get_user_recent_activity_data() {
+		page++;
+		loadTiles(page);
+		$(document).ajaxComplete(function(event, xhr, settings) {
+			if (settings.url.indexOf("recent-activity-data") >=0) {
+				if (more) {
+					page++;
+					loadTiles(page);
+				}
+			}
 		});
 	}
 
@@ -194,8 +294,7 @@
 		document.body.appendChild(document.createElement('script')).innerHTML=s.toString().replace(/([\s\S]*?return;){2}([\s\S]*)}/,'$2');
 	}
 
-// embed other functions used by UI after loading
+	// embed other functions used by UI after loading
 	embedFunction(scrl);
-	//embedFunction(this_or_that_spam);
 
 }(window));
