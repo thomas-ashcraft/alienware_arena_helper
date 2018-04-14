915978e83aaa8cef1e3ac10dd62ac467e395dd77
thomas-ashcraft <ashcraftom@gmail.com>
2018-04-15 00:03:19 +0300

    0.5.0_BETA3
    * Proper voting buttons
    + Automated voting is now available in the sections: Videos, Images,
    News and members profiles.
    * "Open Steam profile" button at members profiles is available again.

--- alienwarearena_helper.user.js
+++ alienwarearena_helper.user.js
@@ -43,10 +43,16 @@
 		.awah-btn-tots {}
 		.awah-btn-cons,
 		.awah-btn-cons:hover {color: gold;}
+		.list-group-item > .awah-btn-cons {width: 50%;}
+		.list-profile-actions > li > .awah-btn-cons {width: 50%;}
+		.awah-btn-cons.disabled::before {content: ''; width: 100%; height: 100%; position: absolute; top: 0; left: 0; background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACAQMAAABIeJ9nAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABlBMVEUAAAAAAAClZ7nPAAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAJcEhZcwAACxIAAAsSAdLdfvwAAAAMSURBVAjXY2hgYAAAAYQAgSjdetcAAAAASUVORK5CYII=');}
+
 		.awah-grey {color: #767676;}
 		.awah-casper-out {overflow: hidden !important; animation: awah-casper-out 0.6s ease-in !important;}
-		[data-awah-tooltip]:after {content: attr(data-awah-tooltip); pointer-events: none; padding: 4px 8px; color: white; position: absolute; left: 0; top: 0%; opacity: 0; font-weight: normal; text-transform: none; font-size: smaller; white-space: pre; box-shadow: 0px 0px 3px 0px #54bbdb; background-color: #0e0e0e; transition: opacity 0.25s ease-out, top 0.25s ease-out; z-index: 1000;}
-		[data-awah-tooltip]:hover:after {top: -115%; opacity: 1;}
+		[data-awah-tooltip] {position: relative;}
+		[data-awah-tooltip]:after {content: attr(data-awah-tooltip); pointer-events: none; padding: 4px 8px; color: white; position: absolute; left: 0; bottom: 0%; opacity: 0; font-weight: normal; text-transform: none; font-size: smaller; white-space: pre; box-shadow: 0px 0px 3px 0px #54bbdb; background-color: #0e0e0e; transition: opacity 0.25s ease-out, bottom 0.25s ease-out; z-index: 1000;}
+		[data-awah-tooltip]:hover:after {bottom: 115%; opacity: 1;}
+		.awah-panel {margin: 20px 0;}
 
 		.awah-grp-con {position: absolute; left: 0%; bottom: 100%; text-align: center; color: white; width: 100%; min-width: 170px; background-color: rgba(0, 0, 0, 0.8); padding: 4px;}
 		.awah-voting-direction-panel > label {display: block; margin: 0; font-weight: normal; position: relative;}
@@ -59,6 +65,9 @@
 		.awah-arp-pts {clear: both; width: 100%}
 		.awah-arp-pts > div {clear: both; width: 100%; background-position: 50% 50%; background-repeat: no-repeat; background-size: 100% 12px;}
 		.awah-arp-pts > div::after {content: ""; display: block; height: 0; clear: both;}
+
+		.awah-daily-reset-timer {min-width: 22%;}
+		.toast-body table tbody > :nth-child(2n) {background: #090909}
 
 		.awah-options-btn {float: left; padding-left: 16px; cursor: pointer; transition: text-shadow 0.25s ease-in-out;}
 		.awah-options-btn:hover {text-shadow: 0px 0px 3px rgba(75, 201, 239, 1), 0px 0px 12px rgba(75, 201, 239, 1); /* animation: awah-breathing-text-neon 2s ease 0s infinite alternate; */}
@@ -79,9 +88,6 @@
 		input.awah-opt-input[type="checkbox"]:checked + div > div {left: calc(100% - 50%);}
 		.awah-opt-input[type="checkbox"] + div > div::before {content: 'ON'; position: absolute; right: 120%;}
 		.awah-opt-input[type="checkbox"] + div > div::after {content: 'OFF'; color: #767676; position: absolute; left: 120%;}
-
-		.awah-daily-reset-timer {min-width: 22%;}
-		.toast-body table tbody > :nth-child(2n) {background: #090909}
 
 		.account-settings-steam div.steam {background-color: #171a21; border-radius: 100px;}
 
@@ -124,6 +130,7 @@
 	votes_content_action = false;
 	votes_content_processing = false;
 	votes_content_gathering = false;
+	votes_content_url = '';
 	var content_to_vote = [];
 	var content_to_check = [];
 	var content_page = 1;
@@ -536,8 +543,7 @@
 
 	function votes_content_get_page() {
 		votes_content_gathering = true;
-		// TODO: link to content id retreive are various for other site sections!
-		$.get("https://eu.alienwarearena.com/esi/featured-tile-data/Image/" + content_page)
+		$.get(votes_content_url + content_page)
 			.done(function(response) {
 				if (response.data.length == 0) {
 					//more = false;
@@ -572,32 +578,77 @@
 		votes_content_get_page();
 	}
 
-	function votes_content_btn_new() {
-		$('<a class="btn btn-default awah-btn-cons awah-up" href="javascript:void(0);" data-awah-tooltip="Make CON votes">' +
+	function votes_content_register_buttons() {
+		$(".awah-btn-cons").on("click", function() {
+			$(".awah-btn-cons").addClass("disabled");
+			if ($(this).data('awah-voting-direction') == "up") {
+				voting_down = false;
+			} else if ($(this).data('awah-voting-direction') == "down") {
+				voting_down = true;
+			}
+			if ($(this).data('awah-content-url') != "") {
+				votes_content_url = $(this).data('awah-content-url');
+			} else {
+				console.log("[AWAH] content url is not provided! Automated voting is not possible!");
+				return;
+			}
+			votes_content_algorithm(); //start algorithm
+		});
+	}
+
+	function votes_content_btn_featured(content_type = 'Image') {
+		$('<div class="panel panel-default awah-panel">' +
+			'<div class="panel-heading"><h3 class="panel-title"><i class="fa fa-wrench"></i> Alienware Arena helper</h3></div>' +
+			'<div class="list-group">' +
+
+			'<div class="list-group-item">' +
+			'<div class="list-group-item-heading" data-awah-tooltip="The ones you see right here">Vote for featured ' + content_type + '</div>' +
+			'<a class="btn btn-default awah-btn-cons" href="javascript:void(0);" data-awah-tooltip="Make CON votes" data-awah-voting-direction="up" ' +
+			'data-awah-content-url="/esi/featured-tile-data/' + content_type + '/">' +
 			'<i class="fa fa-arrow-up"></i> <span class="hidden-xs">UP-votes</span></a>' +
-			'<a class="btn btn-default awah-btn-cons awah-down" href="javascript:void(0);" data-awah-tooltip="Make CON votes">' +
-			'<i class="fa fa-arrow-down"></i> <span class="hidden-xs">DOWN-votes</span></a>').insertAfter("div:has(.panel-default) > a:last-of-type");
-		$(".awah-btn-cons.awah-up").on("click", function() {
-			$(".awah-btn-cons").addClass("hiddeno");
-			voting_down = false;
-			votes_content_algorithm(); //start algorithm
+			'<a class="btn btn-default awah-btn-cons" href="javascript:void(0);" data-awah-tooltip="Make CON votes" data-awah-voting-direction="down" ' +
+			'data-awah-content-url="/esi/featured-tile-data/' + content_type + '/">' +
+			'<i class="fa fa-arrow-down"></i> <span class="hidden-xs">DOWN-votes</span></a>' +
+			'</div>' +
+
+			'<div class="list-group-item"' + (content_type == 'News' ? 'style="display: none;"' : '') + '>' +
+			'<div class="list-group-item-heading" data-awah-tooltip="Every ' + content_type + ' which uploaded to the Alienware Arena.\nExcluding ones that moved to \'featured\' list.\nSorting from fresh ones to old ones.">Vote for newly uploaded ' + content_type + '</div>' +
+			'<a class="btn btn-default awah-btn-cons" href="javascript:void(0);" data-awah-tooltip="Make CON votes" data-awah-voting-direction="up" ' +
+			'data-awah-content-url="/esi/tile-data/' + content_type + '/">' +
+			'<i class="fa fa-arrow-up"></i> <span class="hidden-xs">UP-votes</span></a>' +
+			'<a class="btn btn-default awah-btn-cons" href="javascript:void(0);" data-awah-tooltip="Make CON votes" data-awah-voting-direction="down" ' +
+			'data-awah-content-url="/esi/tile-data/' + content_type + '/">' +
+			'<i class="fa fa-arrow-down"></i> <span class="hidden-xs">DOWN-votes</span></a>' +
+			'</div>' +
+
+			'</div>').insertAfter("div:has(.panel-default) > a:last-of-type");
+		votes_content_register_buttons();
+
+		if(DEBUG) $('<div class="list-group-item">' +
+			'<a class="btn btn-default awah-btn-test" href="javascript:void(0);" data-awah-tooltip="At your own risk!">' +
+			'<i class="fa fa-terminal"></i> <span class="hidden-xs">Make test</span></a></div>').appendTo(".awah-panel > .list-group");
+		if(DEBUG) $(".awah-btn-test").on("click", function() {
+			votes_content_cur = getRandomInt(5, 15);
+			votes_content_gathering = true;
+			content_to_check = new Array(votes_content_cur);
+			content_to_vote = new Array(votes_content_cur);
+			arp_pts_status_update();
+			content_to_check = new Array();
+			content_to_vote = new Array();
 		});
-		$(".awah-btn-cons.awah-down").on("click", function() {
-			$(".awah-btn-cons").addClass("hiddeno");
-			voting_down = true;
-			votes_content_algorithm(); //start algorithm
-		});
-		if(DEBUG) $('<a class="btn btn-default awah-btn-test" href="javascript:void(0);">' +
-			 '<i class="fa fa-terminal"></i> <span class="hidden-xs">Make test</span></a>').insertAfter("div:has(.panel-default) > a:last-of-type");
-		 if(DEBUG) $(".awah-btn-test").on("click", function() {
-			 votes_content_cur = getRandomInt(5, 15);
-			 votes_content_gathering = true;
-			 content_to_check = new Array(votes_content_cur);
-			 content_to_vote = new Array(votes_content_cur);
-			 arp_pts_status_update();
-			 content_to_check = new Array();
-			 content_to_vote = new Array();
-		});
+	}
+
+	function votes_content_btn_profile() {
+		$('<li>' +
+			'<a class="btn btn-default awah-btn-cons" href="javascript:void(0);" data-awah-tooltip="Make CON votes" data-awah-voting-direction="up" ' +
+			'data-awah-content-url="/esi/recent-activity-data/user/' + profileData.profile.id + '/">' +
+			'<i class="fa fa-arrow-up"></i> <span class="hidden-xs">UP-votes</span></a>' +
+			'<a class="btn btn-default awah-btn-cons" href="javascript:void(0);" data-awah-tooltip="Make CON votes" data-awah-voting-direction="down" ' +
+			'data-awah-content-url="/esi/recent-activity-data/user/' + profileData.profile.id + '/">' +
+			'<i class="fa fa-arrow-down"></i> <span class="hidden-xs">DOWN-votes</span></a>' +
+			'</div>' +
+			'</li>').appendTo(".list-profile-actions");
+		votes_content_register_buttons();
 	}
 
 	function votes_content_make() {
@@ -745,11 +796,8 @@
 
 	// USER profile functions
 	function show_user_steam_profile_link() {
-		if (profileSteamId > 0) {
-			// fix profileSteamId
-			profileSteamId = $('a[href^="steam://friends/message/"]').prop("href").replace(/steam:\/\/friends\/message\//, "");
-
-			$(".profile-social-links").append('<li data-steam-enabled="true"><span class="fa fa-fw fa-steam-square" style=""></span> <a href="//steamcommunity.com/profiles/' + profileSteamId + '" target="_blank">Visit Steam Profile</a></li>');
+		if (profileData.profile.steamId) {
+			$('<li><a class="btn btn-default btn-block" href="//steamcommunity.com/profiles/' + profileData.profile.steamId + '" target="_blank" data-awah-tooltip="Open user\'s Steam profile in new tab"><span class="fa fa-fw fa-steam"></span> Open Steam profile</a></li>').appendTo(".list-profile-actions");
 		}
 	}
 
@@ -842,10 +890,19 @@
 			break;
 		case /^\/ucf\/Image$/.test(path):
 			if(DEBUG) console.log("SWITCH: Featured images page");
-			votes_content_btn_new();
+			votes_content_btn_featured('Image');
+			break;
+		case /^\/ucf\/Video$/.test(path):
+			if(DEBUG) console.log("SWITCH: Featured videos page");
+			votes_content_btn_featured('Video');
+			break;
+		case /^\/ucf\/News$/.test(path):
+			if(DEBUG) console.log("SWITCH: Featured news page");
+			votes_content_btn_featured('News');
 			break;
 		case /^\/member\/.*$/.test(path):
 			if(DEBUG) console.log("SWITCH: user profile page");
+			votes_content_btn_profile();
 			show_user_steam_profile_link();
 			break;
 		case /\/$/.test(url):
