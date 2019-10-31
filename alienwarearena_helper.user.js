15ee1b06a4f89d70d186d36694e17ac65005b4d1
NoXPhasma <NoXPhasma@users.noreply.github.com>
2019-10-31 14:44:14 +0100

    Add @updateURL for automatic updates

--- alienwarearena_helper.user.js
+++ alienwarearena_helper.user.js
@@ -7,6 +7,7 @@
 // @match        *://*.alienwarearena.com/*
 // @match        *://*.alienwarearena.com//*
 // @license      GPL-2.0-or-later; http://www.gnu.org/licenses/gpl-2.0.txt
+// @updateURL    https://github.com/thomas-ashcraft/alienware_arena_helper/raw/master/alienwarearena_helper.user.js
 // @icon         https://www.alienwarearena.com/favicon.ico
 // @grant        none
 // @noframes
