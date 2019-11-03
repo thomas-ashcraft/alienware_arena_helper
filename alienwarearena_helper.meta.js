8e29f615ef63249c8dfda73b85efb97a82087436
NoXPhasma <noxphasma@live.de>
2019-11-03 10:40:14 +0100

    Add meta.js file

---
+++ alienwarearena_helper.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Alienware Arena helper
+// @namespace    https://github.com/thomas-ashcraft
+// @version      1.1.7
+// @description  Earn daily ARP easily
+// @author       Thomas Ashcraft
+// @match        *://*.alienwarearena.com/*
+// @match        *://*.alienwarearena.com//*
+// @license      GPL-2.0-or-later; http://www.gnu.org/licenses/gpl-2.0.txt
+// @updateURL    https://github.com/thomas-ashcraft/alienware_arena_helper/raw/master/alienwarearena_helper.meta.js
+// @downloadURL  https://github.com/thomas-ashcraft/alienware_arena_helper/raw/master/alienwarearena_helper.user.js
+// @icon         https://www.alienwarearena.com/favicon.ico
+// @grant        none
+// @noframes
+// ==/UserScript==
